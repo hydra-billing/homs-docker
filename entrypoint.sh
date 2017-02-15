68087e270e136b3b44337fca85125d452093c48d
Kirill Lyubchenko <kirill.lyubchenko@gmail.com>
2017-02-15 14:53:53 +0300

    Changed test scenario for existence and filling of config files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,13 @@
 
 cp -rn /tmp/config/* /opt/homs/config/
 
-if [[ ! -a config/activiti.yml || ! -a activiti.lock ]]; then
-	cp config/activiti.yml.sample config/activiti.yml
-	[[ -n $ACTIVITI_HOST ]] && sed -i -e "s/localhost/$ACTIVITI_HOST/" config/activiti.yml
-	[[ -n $ACTIVITI_USER ]] && sed -i -e "s/user/$ACTIVITI_USER/" config/activiti.yml
-	[[ -n $ACTIVITI_PASS ]] && sed -i -e "s/changeme/$ACTIVITI_PASSWORD/" config/activiti.yml
-	touch activiti.lock
-fi
+cp -n config/activiti.yml.sample config/activiti.yml
+[[ -n $ACTIVITI_HOST ]] && sed -i -e "s/localhost/$ACTIVITI_HOST/" config/activiti.yml
+[[ -n $ACTIVITI_USER ]] && sed -i -e "s/user/$ACTIVITI_USER/" config/activiti.yml
+[[ -n $ACTIVITI_PASS ]] && sed -i -e "s/changeme/$ACTIVITI_PASSWORD/" config/activiti.yml
 
-if [[ ! -a config/database.yml || ! -a db.lock ]]; then
-	cp config/database.yml.sample config/database.yml
-	[[ -n $DB_HOST ]] && sed -i -e "s/localhost/$DB_HOST/" config/database.yml
-	touch db.lock
-fi
+cp -n config/database.yml.sample config/database.yml
+[[ -n $DB_HOST ]] && sed -i -e "s/localhost/$DB_HOST/" config/database.yml
 
 bundle exec rake db:migrate
 
